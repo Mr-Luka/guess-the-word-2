6d90de0736f2f2cbdbda97a51f57977956a47eaa
Luka <luka.luka.luka93@gmail.com>
2024-06-24 17:33:06 -0700

    selected the elements in javascript

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
-// Add some interactivity to the website
-console.log("Hello, world!");
+const scramblleWord = document.querySelector("#scramblle-word");
+const tries = document.querySelector("#span-tries");
+const triesSymbol = document.querySelector("p#tries");
+const mistakes = document.querySelector("#span-mistakes");
+const input = document.querySelectorAll("input");
+
+const random = document.querySelector("#random");
+const reset = document.querySelector("#reset");
+
+console.log(random);
